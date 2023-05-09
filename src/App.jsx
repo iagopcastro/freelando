66610f3970bd39178b297627d38c2b4955d44484
Iago Castro <iagopcastro@hotmail.com>
2023-05-08 23:51:39 -0300

    instalação do emotion e estilização inicial

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Estilos } from "./components/EstilosGlobais/Estilos";
+
+function App() {
+  return (
+    <div>
+      <Estilos />
+        <h1>Freelando</h1>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/EstilosGlobais/Estilos.jsx
@@ -0,0 +1,14 @@
+import { Global, css } from '@emotion/react'
+
+const estilos = css`
+    * {
+        font-family: 'Montserrat', sans-serif;
+    }
+`
+
+export const Estilos = () => {
+    return(
+    <Global styles={estilos}
+    />
+    )
+}
